1d4091d4e6d61d71eea71bb0a18d12294d4f1c29
sakuramoto_nanaka <sakuramoto.nanaka@alhinc.jp>
2025-03-24 16:32:22 +0900

    「WEB開発基礎」「応用編　簡易Twitter」「打鍵テスト」まで完了

--- src/chapter6/controller/TopServlet.java
+++ src/chapter6/controller/TopServlet.java
@@ -1,6 +1,7 @@
 package chapter6.controller;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.logging.Logger;
 
 import javax.servlet.ServletException;
@@ -9,35 +10,48 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import chapter6.beans.User;
+import chapter6.beans.UserMessage;
 import chapter6.logging.InitApplication;
-
+import chapter6.service.MessageService;
 
 @WebServlet(urlPatterns = { "/index.jsp" })
 public class TopServlet extends HttpServlet {
 
+    private static final long serialVersionUID = 1L;
 
-	/**
-	* ロガーインスタンスの生成　●ログが出力できるようにするための実装
-	*/
-	Logger log = Logger.getLogger("twitter");
+    /**
+    * ロガーインスタンスの生成
+    */
+    Logger log = Logger.getLogger("twitter");
 
-	/**
-	* デフォルトコンストラクタ
-	* アプリケーションの初期化を実施する。
-	*/
-	public TopServlet() {
-		InitApplication application = InitApplication.getInstance();
-		application.init();
+    /**
+    * デフォルトコンストラクタ
+    * アプリケーションの初期化を実施する。
+    */
+    public TopServlet() {
+        InitApplication application = InitApplication.getInstance();
+        application.init();
 
-	}
+    }
 
-	@Override
-	protected void doGet(HttpServletRequest request, HttpServletResponse response)
-			throws IOException, ServletException {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws IOException, ServletException {
 
-		//●実際にログを出力
-		log.info(new Object(){}.getClass().getEnclosingClass().getName() +
-		" : " + new Object(){}.getClass().getEnclosingMethod().getName());
-		request.getRequestDispatcher("/top.jsp").forward(request, response);
-	}
+	  log.info(new Object(){}.getClass().getEnclosingClass().getName() + 
+        " : " + new Object(){}.getClass().getEnclosingMethod().getName());
+
+        boolean isShowMessageForm = false;
+        User user = (User) request.getSession().getAttribute("loginUser");
+        if (user != null) {
+            isShowMessageForm = true;
+        }
+
+        List<UserMessage> messages = new MessageService().select();
+
+        request.setAttribute("messages", messages);
+        request.setAttribute("isShowMessageForm", isShowMessageForm);
+        request.getRequestDispatcher("/top.jsp").forward(request, response);
+    }
 }
